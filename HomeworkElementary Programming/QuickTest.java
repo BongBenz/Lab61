69b3eab64e88c058a110b0f8ea384a12e27eebed
BongBenz <BongBenzRec@Gmaiil.com>
2019-05-24 02:28:44 +0700

    add directory HomeworkElementary Programming

---
+++ HomeworkElementary Programming/QuickTest.java
@@ -0,0 +1,10 @@
+
+public class QuickTest {
+	//Exercise 10
+	public static void main(String[] args) {
+		double sum;
+		sum = (32.6 + 55.2 + 67.9 + 48.6)/4;
+		System.out.println("Average : "+sum);
+	}
+
+}
---
+++ HomeworkElementary Programming/SubTotals.java
@@ -0,0 +1,22 @@
+
+public class SubTotals {
+	//Exercise 17
+	public static void main(String[] args) {
+		int sum;
+		   sum = 0;
+		   System.out.println("The value of sum is initially set to " + sum);
+		   
+		   sum = sum + 96;
+		   System.out.println("  Sum is now "  + sum );
+		   
+		   sum = sum + 70;
+		   System.out.println("  Sum is now "  + sum );
+		   
+		   sum = sum + 85;
+		   System.out.println("  Sum is now "  + sum );
+		   
+		   sum = sum + 60;
+		  System.out.println("The final sum is  "  + sum );
+	}
+
+}
